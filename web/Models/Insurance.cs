535c9112ff9f48bde803b71e0ad73ea3d8290a2f
juan <paisaroloco@gmail.com>
2020-08-07 14:55:04 -0500

    change controllers and views

---
+++ web/Models/Insurance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InsuranceTest.Models
+{
+    public class Insurance
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public DateTime coverageInit { get; set; }
+        [Required]
+        public int coverageTime { get; set; }
+        public float price { get; set; }
+        public string riskType { get; set; }
+        public float coverage { get; set;}
+        public string insuranceType { get; set; }
+    }
+}
